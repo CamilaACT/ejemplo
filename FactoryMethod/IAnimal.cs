34e85e0c15ec8300bd96523360f9615d3ef81a01
Jhon Christian Ambrad <jhonchristian.ambrad@cit.edu>
2022-11-08 14:28:18 +0800

    Add sample

---
+++ FactoryMethod/IAnimal.cs
@@ -0,0 +1,7 @@
+﻿namespace FactoryMethod
+{
+    public interface IAnimal
+    {
+        public string Render();
+    }
+}
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FactoryMethod;
+
+int ParkAnimalLimit = 10;
+GeneratePark();
+
+
+void GeneratePark()
+{
+    Console.WriteLine("Generating park!");
+    Random r = new Random();
+    
+    var numAnimals = 0;
+    while (numAnimals < ParkAnimalLimit)
+    {
+        IAnimal animal;
+        var rInt = r.Next(0, 2);
+
+        switch (rInt)
+        {
+            case 0:
+                animal = new Cow();
+                break;
+            case 1:
+                animal = new Bird();
+                break;
+            case 2:
+                animal = new Snake();
+                break;
+            default:
+                animal = new Cow();
+                break;
+        }
+
+        numAnimals++;
+        Console.WriteLine(animal.Render());
+    }
+}
